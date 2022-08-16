89d3dabbbc5fd52e2a137a0e5b71bb1f1fc8af31
Rafhael Lima Cançado <rafhael.cancado@gmail.com>
2022-08-16 15:04:33 -0300

    Rotação de Cartas onclick

---
+++ script.js
@@ -0,0 +1,4 @@
+function rotateCard(element){
+    element.querySelector(".front-face").classList.toggle("rotate-front");
+    element.querySelector(".back-face").classList.toggle("rotate-back");
+}
